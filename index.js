bead968d572e5091600aee07e403346ebdf27606
Bradley Troll <bradleytroll@gmail.com>
2023-05-24 12:20:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+function submitData(name, email) {
+   let requestData = {
+    method: 'POST',
+    headers: {
+        "content-type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email
+    })
+   }
+   return fetch("http://localhost:3000/users", requestData)
+   .then(function(response) {
+       return response.json();
+   })
+   .then(function (object) {
+       document.body.innerHTML = object.id
+   })
+   .catch(function(error) {
+       document.body.innerHTML = error.message
+   });
+};
+
